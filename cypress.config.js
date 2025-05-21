b206d7a7a04139612d4bb4b54a8a561fbbab415c
unknown <deamgaudencio@gmail.com>
2025-05-20 22:21:46 -0300

    ajuste no env

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://api.trello.com/1",
     env:{
-
-      API_KEY:"88872f9b0c6eac33d7f93114392c5dde",
-      API_TOKEN:"ATTA510585a9c9e5b2ebaa474aff875b6e7c57ce98a81189edc200b28989f9df9421DD003E7A"
+      API_KEY:"xxx",
+      API_TOKEN:"xxx"
     },
     setupNodeEvents(on, config) {
      
